c32d10cc45e7c82ceaf43423239f36de54bd0367
Alexander Quang <alexquang1991@gmail.com>
2016-03-12 09:36:03 -0800

    first commit

---
+++ awesome_module.py
@@ -0,0 +1,4 @@
+import sys
+class awesome_module:
+        def awesome_module():
+            print()
---
+++ new_demo_runner.py
@@ -0,0 +1,4 @@
+from new_demo.new_demo import awesome_class
+if __name__ == '__main__':
+    a = awesome_class()
+    a.awesome_function()
